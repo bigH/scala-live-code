049a2a221e4894b706c7b9fe451dbe04f8421822
Hiren Hiranandani <hiren@webs.com>
2014-03-05 11:01:21 -0500

    Removing un-needed libs.

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -3,22 +3,11 @@ import sbt._
 object Libraries {
 
   object main {
-    def slf4jModule(name: String) = "org.slf4j" % name % Versions.slf4j
-
-    lazy val scopt = "com.github.scopt" %% "scopt" % "2.1.0"
-    object guice {
-      lazy val core = "com.google.inject" % "guice" % Versions.guice
-      lazy val multiBindings = "com.google.inject.extensions" % "guice-multibindings" % Versions.guice
-    }
-
   }
 
   object test {
     lazy val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalatest
-    lazy val specs2 = "org.specs2" %% "specs2" % Versions.specs2
-    lazy val mockito = "org.mockito" % "mockito-all" % Versions.mockito
   }
-
 
   lazy val defaultTestingLibs = {
     import test._
@@ -26,9 +15,7 @@ object Libraries {
     def inTest(module: ModuleID) = module % "test"
 
     Seq (
-      inTest(scalaTest),
-      inTest(specs2),
-      inTest(mockito)
+      inTest(scalaTest)
     )
   }
 
